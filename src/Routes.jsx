9802bd4703040457ad01d7f0c715ebf47e9ba6d7
felipehdev <felipehdev@gmail.com>
2022-08-01 17:08:38 -0300

    criado gerenciador de rotas com react router dom

---
+++ src/Routes.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter, Routes as Switch, Route } from 'react-router-dom'
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      
+
+    <Switch>
+      
+
+    </Switch>
+
+
+    </BrowserRouter>
+  )
+}
+
+export default Routes
