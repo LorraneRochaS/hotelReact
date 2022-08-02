2c68201676569b195a95f291117a45c4e445f77f
felipehdev <felipehdev@gmail.com>
2022-08-02 16:12:57 -0300

    adicionado reset css

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Routes from './Routes'
+import './style/reset.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
