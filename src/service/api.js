bc217fb3f8638e558a2207286c19d4df748c4392
nahlavalentina <nahlavalentinas@gmail.com>
2022-08-08 14:25:24 -0300

    created get methods, pending change on hotel api

--- src/service/api.js
+++ src/service/api.js
@@ -5,9 +5,20 @@ const instancia = axios.create({
     baseURL: "https://api-site-hotel.herokuapp.com/"
 })
 
+
+// função asíncrona para receber todos os quartos
+
 export const getRooms = async () => {
-    const response = await instancia.get("/quarto");
+    const response = await instancia.get('/quarto');
       const json = await response.data;
       console.log(json);
       return json;
+}
+
+// função assíncrona para receber quartos por quantidade de hóspedes (precisa adicionar método get na API por categoria)
+
+export const getRoomsByCapacity = async (capacity) => {
+    const response = await instancia.get(`/quarto/${capacity}`)
+    const json = await response.data;
+    return json;
 }
