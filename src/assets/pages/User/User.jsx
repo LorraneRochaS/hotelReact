baacf267f79664c454152672ea424a1bb23cf65a
nahlavalentina <nahlavalentinas@gmail.com>
2022-08-09 09:15:30 -0300

    change to user page

--- src/assets/pages/User/User.jsx
+++ src/assets/pages/User/User.jsx
@@ -6,18 +6,21 @@ import Booking from '../../components/Booking/Booking'
 import SearchBar from '../../components/SearchBar/SearchBar'
 import Title from '../../components/Title/Title'
 import { getBookingsById } from '../../../service/api'
+import { getBookings } from '../../../service/api'
 
 const User = () => {
 
     //erro ao buscar API. Método p/ buscar por id de usuário?
 
     const params = useParams();
-    const { ID_USUARIO } = params;
-  const [bookings, setBookings] = useState();
+    // const { ID_RESERVA } = params;
+  const [reservas, setReservas] = useState();
 
   async function request() {
-    const response = await getBookingsById()
-    setBookings(response);
+    const response = await getBookings()
+    //Array vindo vazio com busca por ID
+    // const response = await getBookingsById(params.reserva)
+    setReservas(response);
   }
 
   useEffect(() => {
@@ -31,23 +34,23 @@ const User = () => {
       <h3>Nova Reserva</h3>
       <SearchBar />
       <h3>Últimas Reservas:</h3>
-      {/* <div>{!!bookings &&
-        bookings.reserva.map((booking, key) => {
+      <div>{!!reservas &&
+        reservas.reserva.map((reserva, key) => {
           return (
 
-            //Mudar nome dos atributos para fazer funcionar
+            //Mudar nome dos atributos no cdg para fazer funcionar?
             <Booking
               src="https://cf.bstatic.com/images/hotel/840x460/631/63184497.jpg"
               descr="img quarto"
-              key={key}
-              idRoom={booking.ID_QUARTO}
-              checkOut={booking.CHECK_OUT}
-              checkIn={booking.CHECK_IN}
-              id={booking.ID_RESERVA}
+              key={reserva.ID_RESERVA}
+              idRoom={reserva.ID_QUARTO}
+              checkOut={reserva.CHECKOUT}
+              checkIn={reserva.CHECKIN}
+              id={reserva.ID_RESERVA}
             />
           );
         })}
-    </div> */}
+    </div>
 
     </div>
   )
