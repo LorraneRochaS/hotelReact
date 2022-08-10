62e58f84c332833a3d20da8d6b8ddf95d1526fcb
nahlavalentina <nahlavalentinas@gmail.com>
2022-08-10 05:50:30 -0300

    deleted form component, adjustments

--- src/pages/Reserva/Reserva.jsx
+++ src/pages/Reserva/Reserva.jsx
@@ -2,10 +2,9 @@ import React from "react";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
-import Title from '../../components/Title/Title'
+import Title from "../../components/Title/Title";
 import { getBookingByBookingId } from "../../service/api";
 import { deleteBooking } from "../../service/api";
-import Form from "../../components/Form/Form";
 import Button from "../../components/Button/Button";
 
 const Reserva = () => {
@@ -20,8 +19,8 @@ const Reserva = () => {
   }
 
   function handleDelete() {
-    deleteBooking()
-    }
+    deleteBooking();
+  }
 
   useEffect(() => {
     request();
@@ -39,19 +38,64 @@ const Reserva = () => {
             return (
               //Mudar nome dos atributos no cdg para fazer funcionar?
               <div>
-                <Form
-                key={key}
-                quarto={reserva.ID_QUARTO}
-                hospede={reserva.ID_HOSPEDE}
-                checkout={reserva.CHECKOUT}
-                checkin={reserva.CHECKIN}
-                reserva={reserva.ID_RESERVA}
-              />
-              <Button text="atualizar"/>
-              <Button text="deletar" onClick={handleDelete}/>
+                <form action="" key={key}>
+                  <fieldset>
+                    <label htmlFor="">Reserva</label>
+                    <input
+                      type="text"
+                      placeholder="reserva"
+                      value={reserva.ID_RESERVA}
+                      onChange={({ target }) =>
+                        handleChange(target, "ID_RESERVA")
+                      }
+                    />
+                  </fieldset>
+                  <fieldset>
+                    <label htmlFor="">Quarto</label>
+                    <input
+                      type="text"
+                      placeholder="quarto"
+                      value={reserva.ID_QUARTO}
+                      onChange={({ target }) =>
+                        handleChange(target, "ID_QUARTO")
+                      }
+                    />
+                  </fieldset>
+                  <fieldset>
+                    <label htmlFor="">Hóspede</label>
+                    <input
+                      type="text"
+                      placeholder="hospede"
+                      value={reserva.ID_HOSPEDE}
+                      onChange={({ target }) =>
+                        handleChange(target, "ID_HOSPEDE")
+                      }
+                    />
+                  </fieldset>
+                  <fieldset>
+                    <label htmlFor="">Check In:</label>
+                    <input
+                      type="date"
+                      placeholder="checkin"
+                      value={reserva.CHECKIN}
+                      onChange={({ target }) => handleChange(target, "CHECKIN")}
+                    />
+                  </fieldset>
+                  <fieldset>
+                    <label htmlFor="">Checkout:</label>
+                    <input
+                      type="date"
+                      placeholder="checkout"
+                      value={reserva.CHECKOUT}
+                      onChange={({ target }) =>
+                        handleChange(target, "CHECKOUT")
+                      }
+                    />
+                  </fieldset>
+                </form>
+                <Button text="atualizar" />
+                <Button text="deletar" onClick={handleDelete} />
               </div>
-              
-
             );
           })}
       </div>
