4a518e87adb0d335b0951d6d96f02a5319c59cbb
felipehdev <felipehdev@gmail.com>
2022-08-10 03:17:51 -0300

    melhorias no css de diversas pags

--- src/assets/components/Header/Header.jsx
+++ src/assets/components/Header/Header.jsx
@@ -12,13 +12,13 @@ const Header = () => {
       <div className={S.header}>
         <div className={S.headerContent}>
           <Link className={S.title} to="/">
-            Hotel
+            H. FLEN 
           </Link>
           <div className={S.userMenu}>
             <Link className={S.login} to="/login">
-              <Button text="entrar" />
+              <Button text="Entrar" />
             </Link>
-            <UserPicture />
+            <UserPicture src='https://cdn-icons.flaticon.com/png/128/552/premium/552848.png?token=exp=1660107997~hmac=09742289e902617856b57698291a0f39' />
           </div>
         </div>
       </div>
